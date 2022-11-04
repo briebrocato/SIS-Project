cf1e96cbe5ab05981f63f47e203578f3be9b54f6
brie.brocato <brie.brocato@MU-C1-18.mullen.pvt.k12.co.us>
2022-11-04 13:16:23 -0600

    sort students class

--- SIS Project/src/SISRunner.java
+++ SIS Project/src/SISRunner.java
@@ -4,21 +4,8 @@ public class SISRunner
 
 		public static void main(String[] args)
 			{
-
-				int num1 = 3;
-		        int num2 = 4;
-		        
-		        double average= (num1 + num2)/2.0;
-		        System.out.println(average);
-		        System.out.println(average);
-		        System.out.println(average);
-		        System.out.println(average);
-		        
-		        System.out.println("What is your favorite animal?");
-		       
-		     
-		        
-		        System.out.println("Matthew is back");
+				
+				
 
 
 			}
