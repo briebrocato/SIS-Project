c2b9634b56c1668bba393cb65149eaebe099deb3
brie.brocato <brie.brocato@MU-C1-18.mullen.pvt.k12.co.us>
2022-11-07 13:57:00 -0700

    calculate GPA class and main menu and sort students

--- SIS Project/src/SISRunner.java
+++ SIS Project/src/SISRunner.java
@@ -4,7 +4,11 @@ public class SISRunner
 
 		public static void main(String[] args)
 			{
-				
+			System.out.println("What would you like to do?");	
+			System.out.println("1) add or delete a student");
+			System.out.println("2) change student grades/schedule");
+			System.out.println("3) sort students");
+			
 				
 
 
