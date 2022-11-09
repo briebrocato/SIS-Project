95050a26019c30fd9e6daf84c020466b0f6da04c
brie.brocato <brie.brocato@MU-C1-18.mullen.pvt.k12.co.us>
2022-11-09 09:49:38 -0700

    sort menu

--- SIS Project/src/SortStudents.java
+++ SIS Project/src/SortStudents.java
@@ -1,11 +1,47 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class SortStudents
 	{
 		public static void sortRoster()
-		{
+			{
+			Scanner userInput = new Scanner(System.in);
+			System.out.println("Sort Students");
+			System.out.println("1) Sort by last name");
+			System.out.println("2) Sort by GPA");
+			System.out.println("3) Sort by period");
+			int answer = userInput.nextInt();
 			
-		}
+			if(answer == 1)
+				{
+					sortLastName();
+				}
+			else if (answer == 2)
+				{
+					sortGPA();
+				}
+			else if(answer == 3)
+				{
+					sortPeriod();
+				}
+			
+
+
+			}
+		
+		public static void sortLastName()
+			{
+				
+			}
+		
+		public static void sortGPA()
+			{
+				
+			}
+		
+		public static void sortPeriod()
+			{
+				
+			}
 		
 		
 	}
